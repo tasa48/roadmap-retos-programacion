abe4fb7740430d5b27b90d81cf0a4368c67ef9e8
tasa48 <125674652+tasa48@users.noreply.github.com>
2024-10-04 18:11:59 -0500

    hola estoyiniciando los retos

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/php/tasa48.php
@@ -0,0 +1,21 @@
+<?php  
+// hola  mando url de la pagina ofcial de php: https://www.php.net/
+
+// me gustaria mejorar mis abilidades de como progrmador al hacer este tipo de retos 
+
+// variables 
+$alfa = "hola mundo";
+// constante  tiene de nombre : beta 
+define ("beta","hola mundo");
+
+$casa = true ;
+$carro = false ;
+$mascotas = 4;
+$cuartos = 1;
+$dinero = 0.2;
+
+$nombreLenguanje = "php";
+$miNombre = "tasa48";
+
+echo "hola me llamo $miNombre y el lenguaje que estoy usando es $nombreLenguaje"
+?>
